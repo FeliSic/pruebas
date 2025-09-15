e6c84d7540da50ce6e48dc4a7b9f48cb717a2ed1
FeliSic <felisicca@gmail.com>
2025-09-15 12:24:49 -0300

    Update db.ts

--- db/db.ts
+++ db/db.ts
@@ -9,7 +9,12 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json());
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"../../src/dist")))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../src/dist/index.html')); // Ajusta la ruta
+});
 
 app.post('/auth', (req, res) => {
   res.json({ message: 'Auth OK' });
